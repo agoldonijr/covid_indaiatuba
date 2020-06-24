7798173c359db5dc8f40ea8d3dd9f23af9edec54
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2020-06-24 18:58:44 -0300

    Covid indaiatuba README.md

--- covid-indaiatuba.py
+++ covid-indaiatuba.py
@@ -16,7 +16,12 @@ with open('./data') as csvfile:
         confirmados.append(row[1])
         mortes.append(row[2])
 
-plt.plot_date(data,confirmados, marker='o')
-plt.plot_date(data,mortes, marker='^', color='red')
+plt.title("Curva Epidemiológica de Indaiatuba") 
+plt.xlabel("Data")
+plt.ylabel("Quantidade")
+plt.annotate(confirmados[-1], xy=(1, confirmados[-1]), xytext=(8, 0), xycoords=('axes fraction', 'data'), textcoords='offset points')
+plt.annotate(mortes[-1], xy=(1, mortes[-1]), xytext=(8, 0), xycoords=('axes fraction', 'data'), textcoords='offset points')
+plt.plot_date(data,confirmados, marker='o',color="blue", ls="-", label='Confirmados')
+plt.plot_date(data,mortes, marker='^', color='red', ls="-", label='Mortes')
+plt.legend(loc="upper left")
 plt.show()
-        
